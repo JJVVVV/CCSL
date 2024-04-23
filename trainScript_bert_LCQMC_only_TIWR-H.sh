318dc406ff7f033448a2917300e2682e69552c57
JIE <1454046732@qq.com>
2024-04-23 16:36:21 +0800

    .

--- trainScript_bert_LCQMC_only_TIWR-H.sh
+++ trainScript_bert_LCQMC_only_TIWR-H.sh
@@ -2,7 +2,7 @@
 
 # nohup ./trainScript_bert_LCQMC_only_TIWR-H.sh > /dev/null 2>&1 &
 
-CUDA_VISIBLE_DEVICES=1
+CUDA_VISIBLE_DEVICES=3
 # CUDA_VISIBLE_DEVICES=0/1/2/3/4/5/6/7
 
 # 定义一个数组，存放可用cuda
@@ -21,13 +21,11 @@ declare -A pid_cuda
 
 
 # all_times=(0.2 0.4 0.6 0.8 1)
-all_times=(0.8)
+all_times=(0.8 0.2 0.4 0.6 1)
 seeds_of_stage1=(42 109 38 62 54)
 seeds_of_stage1=(29 109 38 14 44)
-seeds=(62 11 44 14 30 109 38 54)
-seeds=(1 2 3 4 5 6 7 8 9 10 12 15 17 18 19 20 21 22 23 25 26 27 28 29)
-
-
+seeds=(62 44 11 2 5 6 7 8 9 10 12 15 17 18 19 20 21 22 23 25 26 27 28 29 30 31 32 33 34 35)
+seeds=(8 20 9 35 21 0 66 77)
 
 for times in ${all_times[@]}
 do
@@ -52,7 +50,7 @@ do
 
       model_name="TIWR-H_nodrop_single_model_hardcases_from_baseline_warmboost_fix_num_ratio=${times}/seed_of_stage1=$seed_of_stage1"
       model_name="TWR-H_nodrop_single_model_hardcases_from_baseline_warmboost_fix_num_ratio=${times}/seed_of_stage1=$seed_of_stage1"
-
+      model_name="TWR-H_nodrop_single_model_hardcases_from_baseline_warmboost_fix_num_ratio=${times}_record_pipline/seed_of_stage1=$seed_of_stage1"
       # model_name="TIWR-H_nodrop_single_model_hardcases_from_baseline_warmboost_mix_easycases_totaltimes=${times}/seed_of_stage1=$seed_of_stage1"
 
 
