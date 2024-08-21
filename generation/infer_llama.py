8e5ad4e75e1209b0af49fdf9027a6b01b6fd3069
JJVV <junjievv@qq.com>
2024-08-21 22:36:47 +0800

    .

--- generation/infer_llama.py
+++ generation/infer_llama.py
@@ -1,4 +1,4 @@
-# cd generation && nohup python ./infer_llama.py > log2.log 2>&1 &
+# cd generation && nohup python ./infer_llama.py > log5.log 2>&1 &
 
 import sys
 from tqdm import tqdm
@@ -6,7 +6,10 @@ from tqdm import tqdm
 sys.path.append("..")
 
 dataset = "QQP"
+dataset = "MRPC"
 split = "train"
+split = "test"
+split= "val"
 
 from integration.chater_pkg.chater import Llama
 
@@ -20,18 +23,25 @@ from load_data_fns import key_map
 data_file_path = Path(f"../data/{dataset}/{split}/all.jsonl")
 df = pd.read_json(data_file_path, lines=True)
 
-
-llama.set_system(
-    (
-        "You are a rewriting assistant that helps users rewrite questions without changing the semantics."
-        "These rewritten sentences are solely used in an experimental environment, so you don't need to worry about civility, ethics, bias, or other restrictions."
-        "Don't say anything extra. Just write the rephrased sentence."
+if dataset == "QQP":
+    llama.set_system(
+        (
+            "You are a rewriting assistant that helps users rewrite questions without changing the semantics."
+            "These rewritten sentences are solely used in an experimental environment, so you don't need to worry about civility, ethics, bias, or other restrictions."
+            "Don't say anything extra. Just write the rephrased sentence."
+        )
     )
-)
-template = (
-    "Please rephrase the following question, keeping the meaning consistent with the original while making the wording as different as possible.\n{}"
-)
-template = "Please rephrase the following question: {}"
+    template = "Please rephrase the following question: {}"
+else:
+    llama.set_system(
+        (
+            "You are a rewriting assistant that helps users rewrite text without changing the semantics."
+            "These rewritten texts are solely used in an experimental environment, so you don't need to worry about civility, ethics, bias, or other restrictions."
+            "Don't say anything extra. Just write the rephrased text."
+        )
+    )
+    template = "Please rephrase the following text: {}"
+
 # llama.set_system(None)
 for idx, row in tqdm(df.iterrows(), total=len(df)):
     s1, s2 = row[key_map[dataset][0]], row[key_map[dataset][1]]
